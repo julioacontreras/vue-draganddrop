b5653c9cca8047274297d32be1eb889593c06fd3
Julio Aranis Contreras <julio.contreras@s2g.com>
2019-01-29 17:13:42 +0100

    fix run component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import Vue from 'vue';
 
 import DragAndDrop from './DragAndDrop.vue';
 
-// Install the components
-export function install(Vue) {
-	Vue.component('draganddrop', DragAndDrop);
-}
-
 // Expose the components
 export { DragAndDrop };
 
